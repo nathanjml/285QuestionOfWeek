87419e31183a56fa62bdd1020e0d8fba5b6473d5
Unknown <nathan.murphy@selu.edu>
2019-04-15 19:35:39 -0500

    initial

---
+++ ClassExample/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassExample
+{
+    public static class Extensions
+    {
+        public static (IEnumerable<T>, IEnumerable<T>) Partition<T>(this IEnumerable<T> obj, Func<T, bool> criterion)
+        {
+            //TODO: fix this. What goes here? Hint: use Linq.
+        }
+    }
+}
---
+++ ClassExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var list = new List<int> { 3, 4, -1, 4, 5, 9, 10, 120, 2, 1, 0 };
+
+            //partition should provide 2 collections: 
+            //the first result containing everything less than or equal to 2
+            //the second result containing everything greater than 2.
+            //The usage is provided below.
+            var (result, resulttwo) = list.Partition(x => x <= 2);
+            foreach(var num in result)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("--------");
+            foreach (var num in resulttwo)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
